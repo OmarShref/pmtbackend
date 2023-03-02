3655754cb7e7d0f946504b5f9a7849bcaa412f06
Omar Sharif <mohandesomar@gmail.com>
2023-03-02 14:25:45 +0200

    trying to delete element

--- routes/recordsRouter.js
+++ routes/recordsRouter.js
@@ -21,20 +21,19 @@ router.get("/records", (req, res) => {
 
 // PUT
 router.put("/records", (req, res) => {
-  const body = req.body;
-
   if (!req.user) {
     res.status(204).send();
   } else {
+    const body = req.body;
     const schema = joi.object({
       date: joi.date().required(),
-      money: joi.number().max(10000000000).required(),
+      money: joi.number().positive().max(1000000000000).required(),
       type: joi.string().max(10).required(),
       description: joi.string().max(300).required(),
     });
     const { error } = schema.validate(body);
     if (error) {
-      res.send(error.message);
+      res.status(200).send(error.message);
     } else {
       const newRecord = {
         date: body.date,
@@ -48,13 +47,32 @@ router.put("/records", (req, res) => {
         (err, data) => {
           if (err) throw err;
           if (!data) {
-            res.status(204).send("");
+            res.status(200).send("Error happened please try again");
           } else {
-            res.status(200).send();
+            res.status(201).send();
           }
         }
       );
     }
+  }
+});
+
+router.put("/recorddelete", async (req, res) => {
+  if (!req.user) {
+    res.status(204).send();
+  } else {
+    await Record.updateOne(
+      { _id: req.user.recordID },
+      { $pull: { records: null } },
+      (err, data) => {
+        if (err) throw err;
+        if (!data) {
+          res.status(204).send();
+        } else {
+          res.status(200).send("record set to null");
+        }
+      }
+    );
   }
 });
 
